258761b69bf470c29d075668348fda781f878d48
Krystian <krystian@bpcoders.pl>
2018-02-11 18:39:46 +0100

    init commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log('script');
+window.addEventListener('scroll', function(){
+    let scroll = $(this).scrollTop();
+    let windowHeight = window.innerHeight;
+    let header = $('#header');
+    let code = $('#code');
+    let defaultWidth = 50;
+    if(scroll < windowHeight) {
+        let count = Math.round(((scroll * 100) / windowHeight) / 2);
+
+        let headerWidth = defaultWidth - count;
+        header.css('width', headerWidth + 'vw');
+
+        let codeWidth = defaultWidth + count;
+        code.css('width', codeWidth + 'vw');
+    }
+});
